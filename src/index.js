fb660e05c96dfce6c4290fc2f2917ff7386c2d0a
Panos Mita <panos_mita@epam.com>
2021-09-17 14:41:36 +0100

    ESList configured, airbnb styles added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-require('file-loader?name=[name].[ext]!./index.html');
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './App.scss'
+import './App.scss';
 
+// eslint-disable-next-line import/no-webpack-loader-syntax
+require('file-loader?name=[name].[ext]!./index.html');
+
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
